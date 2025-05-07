d3f0343359384381229adc0fc1fa7a45fea96d86
Haritha-E <harithaeswaran@gmail.com>
2025-05-07 22:30:34 +0530

    url

--- client/src/api.js
+++ client/src/api.js
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
 // Base API URL
-export const API_URL = process.env.REACT_APP_API_URL || 'http://localhost:5000/api';
+export const API_URL = `${process.env.REACT_APP_API_URL || 'http://localhost:5000'}/api`;
 
 // 🔐 Helper to get token
 const getAuthHeaders = () => {
